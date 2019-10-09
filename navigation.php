c43558dbbbdc22babf4c9600c5d52faef3a891a0
Charlie <aplik987@gmail.com>
2019-10-09 19:40:04 +0200

    Inicjalizacja strony

---
+++ navigation.php
@@ -0,0 +1,11 @@
+<?php
+echo 
+'<div class="sidenav">
+	<a href="index.php" title="Strona główna">Strona główna</a>
+	<a href="zadanie1.php" title="Zadania 1">Zadanie 1</a>
+	<a href="https://home.pl/hosting" title="Zadanie 2">Zadanie 2</a>
+	<a href="https://home.pl/poczta" title="Zadanie 3">Zadanie 3</a>
+	<a href="https://home.pl/sklepy" title="Zadanie 4">Zadanie 4</a>
+	<a href="https://home.pl/ssl" title="Zadanie 5">Zadanie 5</a>
+</div>';
+?>
---
+++ zadanie1.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Appel Karol - Zadanie 1</title>
+	<link href="mainstyle.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	<?php include 'navigation.php'; ?>
+
+	<div class="content">
+		Zadanie 1
+	</div>
+</body>
+</html>
