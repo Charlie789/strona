f28e3583f08033d813cd350f90c40cf8b3ade0c1
Charlie <admin@kappel.org.pl>
2019-10-10 15:53:23 +0200

    downtime

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Appel Karol</title>
+    <title>Appel</title>
     <link href="mainstyle.css" rel="stylesheet" type="text/css" />
 </head>
 
--- zadanie1.php
+++ zadanie1.php
@@ -1,7 +1,8 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Appel Karol - Zadanie 1</title>
+	<meta http-equiv="refresh" content="10" />
+	<title>Appel</title>
 	<link href="mainstyle.css" rel="stylesheet" type="text/css" />
 </head>
 
@@ -11,18 +12,20 @@
 	<div class="content">
 		Zadanie 1
 		<?php include 'db_connect.php';
-			/*$result = $mysqli->query("SELECT * FROM 
-'LiveTest'") or die ("Błąd zapytania do bazy: $dbname");*/
+			/*$result = $mysqli->query("SELECT * FROM 'LiveTest'") or die ("Błąd zapytania do bazy: $dbname");*/
 			print "<TABLE CELLPADDING=5 BORDER=1>";
-			print "<TR><TD>idt</TD><TD>Nazwa</TD><TD>Status</TD></TR>\n";
+			print "<TR><TD>idt</TD><TD>Nazwa</TD><TD>Status</TD><TD>Downtime</TD></TR>\n";
 			if ($result = $mysqli->query("SELECT * FROM LiveTest")) {
 				printf("<br />Select returned %d rows.<br />", $result->num_rows);
-			while($row = $result->fetch_assoc()) {
+				while($row = $result->fetch_assoc()) {
 					$idt = $row["ID"];
 					$naz = $row["Nazwa"];
+					$tim = $row["DownTime"];
 					$fp = @fsockopen($naz, 80);
-					if ($fp) { $status = 'OK'; } else { $status = 'słabo'; }
-					print "<TR><TD>$idt</TD><TD>$naz</TD><TD>$status</TD></TR>\n";
+					if ($fp) { $status = 'OK'; } else { $status = 'słabo';
+						$tim = strtotime('+ 10 seconds',$tim);
+					/*$mysqli->query('SELECT TIMESTAMPADD(SECOND, 10 ,$tim)');*/ }
+					print "<TR><TD>$idt</TD><TD>$naz</TD><TD>$status</TD><TD>$tim</TD></TR>\n";
 				}
 			}
 			print "</TABLE>";
