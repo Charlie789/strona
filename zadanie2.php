a3ceec332193541df7cce8146e6a8fafb97beccf
Charlie <aplik987@gmail.com>
2019-10-17 14:35:53 +0200

    Dodanie skryptu netstat, przygotowanie podstrony dla zadania 2

---
+++ zadanie2.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="refresh" content="10" />
+	<title>Appel</title>
+	<link href="mainstyle.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	<?php include 'navigation.php'; ?>
+
+	<div class="content">
+		Zadanie 2 <br />
+		<a href="https://github.com/netstat-php/netstat_php">Link do netstat</a>
+		<?php include 'db_connect.php';
+			
+		?>
+	</div>
+</body>
+</html>
