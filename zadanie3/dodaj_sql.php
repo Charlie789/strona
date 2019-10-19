e2772763ff77a0cd4deaf4ea43c6a72671525d5c
Charlie <admin@kappel.org.pl>
2019-10-19 16:07:01 +0200

    zmiana kolejności wywoływania headera; poprawienie braku '

--- zadanie3/dodaj_sql.php
+++ zadanie3/dodaj_sql.php
@@ -1,13 +1,14 @@
 <?php
+	header("Location: konwersacja_sql.php");
+
 	$user = $_POST['user'];
 	$post = $_POST['post'];
 	
 	include 'db_connect.php';
 
-	if($mysqli->query("INSERT INTO `komunikaty`(`nick`, `data`, `komunikat`) VALUES ('$user, NOW(), '$post')") === TRUE) {
+	if($mysqli->query("INSERT INTO `komunikaty`(`nick`, `data`, `komunikat`) VALUES ('$user', NOW(), '$post')") === TRUE) {
 		echo "dodano do bazy danych";
 	} else {
 		echo "Error: " . $mysqli->error;
 	}
-	header("Location: konwersacja.php");
 ?>
