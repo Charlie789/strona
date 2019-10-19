45e4f3d984de341e5fbe602daf0e68379dc75104
Charlie <admin@kappel.org.pl>
2019-10-19 11:15:33 +0200

    zmiana struktury katalogów, oraz poprawienie ścieżek względnych

--- navigation.php
+++ navigation.php
@@ -1,14 +1,14 @@
 <?php
 echo 
 '<div class="sidenav">
-	<a href="index.php" title="Strona główna">Strona główna</a>
-	<a href="zadanie1.php" title="Zadania 1">Zadanie 1</a>
+	<a href="/index.php" title="Strona główna">Strona główna</a>
+	<a href="/zadanie1.php" title="Zadanie 1">Zadanie 1</a>
 	<button type="button" class="collapsible">Zadanie 2</button>
 	<div class="collapse_content">
-		<a href="zadanie2_1.php" title="Zadanie 2_1">Zadanie 2_1</a>
-		<a href="zadanie2_2.php" title="Zadanie 2_2">Zadanie 2_2</a>
+		<a href="/zadanie2/zad2_netstat.php" title="Zadanie 2 Netstat">Netstat</a>
+		<a href="/zadanie2/zad2_goscie.php" title="Zadanie 2 Goscie portalu">Goście Portalu</a>
 	</div>	
 </div>
 
-<script src="js/collapse.js"></script>';
+<script src="/js/collapse.js"></script>';
 ?>
--- zadanie2/zad2_netstat.php
+++ zadanie2/zad2_netstat.php
@@ -1,24 +1,4 @@
 <?php
-/*<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta http-equiv="refresh" content="10" />
-	<title>Appel</title>
-	<link href="mainstyle.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
-	<?php include 'navigation.php'; ?>
-
-	<div class="content">
-		Zadanie 2 <br />
-		<a href="https://github.com/netstat-php/netstat_php">Link do netstat</a>
-		<?php include 'db_connect.php';
-			
-		?>
-	</div>
-</body>
-</html>*/
 /**
  * netstat.php - Show online status of hosts and services
  *
@@ -172,6 +152,7 @@ h1 { font-size: 150%; line-height: 1.2em; margin: .7em 0 .6em 0; }
 --></style>
 </head>
 <body>
+<?php include='navigation.php'; ?>
 <div id="container">
 EOH;
 // end of $htmlheader_tmp
