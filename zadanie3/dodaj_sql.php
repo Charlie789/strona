9026f80a91a8d6eece94963a70935938120cc3d6
Charlie <aplik987@gmail.com>
2019-10-19 15:58:59 +0200

    Dodanie brakujące include do łączenia się z bazą danych

--- zadanie3/dodaj_sql.php
+++ zadanie3/dodaj_sql.php
@@ -2,6 +2,8 @@
 	$user = $_POST['user'];
 	$post = $_POST['post'];
 	
+	include 'db_connect.php';
+
 	if($mysqli->query("INSERT INTO `komunikaty`(`nick`, `data`, `komunikat`) VALUES ('$user, NOW(), '$post')") === TRUE) {
 		echo "dodano do bazy danych";
 	} else {
