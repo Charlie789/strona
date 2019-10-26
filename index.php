de1ec742d2af121f2b401ccc7f820ca3a9df8754
Charlie <aplik987@gmail.com>
2019-10-26 10:35:34 +0200

    Formatowanie

--- index.php
+++ index.php
@@ -2,26 +2,26 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Appel</title>
-    <link href="css/mainstyle.css" rel="stylesheet" type="text/css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Appel</title>
+	<link href="css/mainstyle.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 
 	<?php include 'navigation.php'; ?>
-	
+
 	<div class="content">
 		Strona główna
 		</br>
 		<?php /*include 'db_connect.php';
-		if ($result = $mysqli->query("SELECT * FROM test_table")) {
-    			printf("<br />Select returned %d rows.<br />", $result->num_rows);
-			while($row = $result->fetch_assoc()) {
-        			echo "id: " . $row["ID"]. " - Name: " . $row["Name"]. " " . $row["Surname"]. " - Age: " . $row["Age"]. "<br>";
-    			}
-    		$result->close();
-		}*/
+			if ($result = $mysqli->query("SELECT * FROM test_table")) {
+				printf("<br />Select returned %d rows.<br />", $result->num_rows);
+				while($row = $result->fetch_assoc()) {
+					echo "id: " . $row["ID"]. " - Name: " . $row["Name"]. " " . $row["Surname"]. " - Age: " . $row["Age"]. "<br>";
+				}
+			$result->close();
+			}*/
 		?>
 	</div>
 
