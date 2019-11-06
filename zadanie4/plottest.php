8300af9b4ad89c1a430688a95ae0014068dd6a46
Charlie <admin@kappel.org.pl>
2019-11-06 20:44:50 +0100

    poprawienie wykresu testowego

--- zadanie4/plottest.php
+++ zadanie4/plottest.php
@@ -1,23 +1,27 @@
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Appel</title>
-	<link href="/css/mainstyle.css" rel="stylesheet" type="text/css" />
-	<?php require 'phplot.php'; ?>
-</head>
+<?php
+require_once 'phplot.php';
+$data = array(
+  array('', 1800,   5), array('', 1810,   7), array('', 1820,  10),
+  array('', 1830,  13), array('', 1840,  17), array('', 1850,  23),
+  array('', 1860,  31), array('', 1870,  39), array('', 1880,  50),
+  array('', 1890,  63), array('', 1900,  76), array('', 1910,  92),
+  array('', 1920, 106), array('', 1930, 123), array('', 1940, 132),
+  array('', 1950, 151), array('', 1960, 179), array('', 1970, 203),
+  array('', 1980, 227), array('', 1990, 249), array('', 2000, 281),
+);
 
-<body>
-	<?php include($_SERVER['DOCUMENT_ROOT'].'/navigation.php'); ?>
+$plot = new PHPlot(800, 600);
+$plot->SetImageBorderType('plain');
 
-	<div class="content">
-		<h1>Zadanie 4</h1>
-		<?php
-			$data = array(array('', 10), array('', 1));
-			$plot = new PHPlot();
-			$plot->SetDataValues($data);
-			$plot->SetTitle('First Test Plot');
-			$plot->DrawGraph();
-		?>
-	</div>
-</body>
-</html>
+$plot->SetPlotType('lines');
+$plot->SetDataType('data-data');
+$plot->SetDataValues($data);
+
+# Main plot title:
+$plot->SetTitle('US Population, in millions');
+
+# Make sure Y axis starts at 0:
+$plot->SetPlotAreaWorld(NULL, 0, NULL, NULL);
+
+$plot->DrawGraph();
+?>
