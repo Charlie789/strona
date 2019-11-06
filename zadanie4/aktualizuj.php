afc4911ad2aa1499e89fac67a6863036bc6a21b1
Karol Appel <aplik987@gmail.com>
2019-11-06 21:10:37 +0100

    Poprawienie kwerendy do aktualizacji danych

--- zadanie4/aktualizuj.php
+++ zadanie4/aktualizuj.php
@@ -9,7 +9,7 @@
 
     include 'db_connect.php';
 
-    if($mysqli->query("INSERT INTO `pomiary`(`x1`, `x2`, `x3`, `x4`, `x5`, `data_godzina`) VALUES ('$x1', '$x2', '$x3', '$x4', '$x5', NOW())") === TRUE) {
+    if($mysqli->query("UPDATE `pomiary` SET `x1` = '$x1', `x2` = '$x2', `x3` = '$x3', `x4` = '$x4', `x5` = '$x5', `data_godzina` = NOW() WHERE `id` = 0") === TRUE) {
         echo "dodano do bazy danych";
     } else {
         echo "Error: " . $mysqli->error;
