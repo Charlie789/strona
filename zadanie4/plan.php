2515e70a8a2dbc6e831dfbb9647ffbc07e4957d6
Charlie <aplik987@gmail.com>
2019-11-07 20:33:09 +0100

    test wykresów z javascript

--- zadanie4/plan.php
+++ zadanie4/plan.php
@@ -3,6 +3,30 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Appel</title>
 	<link href="/css/mainstyle.css" rel="stylesheet" type="text/css" />
+	<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+	<script type="text/javascript">
+		window.onload = function () {
+			var chart = new CanvasJS.Chart("chartContainer", {
+				title:{
+					text: "My First Chart in CanvasJS"
+				},
+				data: [
+				{
+					// Change type to "doughnut", "line", "splineArea", etc.
+					type: "column",
+					dataPoints: [
+						{ label: "apple",  y: 10  },
+						{ label: "orange", y: 15  },
+						{ label: "banana", y: 25  },
+						{ label: "mango",  y: 30  },
+						{ label: "grape",  y: 28  }
+					]
+				}
+				]
+			});
+			chart.render();
+		}
+	</script>
 </head>
 
 <body>
@@ -47,6 +71,7 @@
 				<?php echo end($data)[5]; ?>
 			</div>
 		</div>
+		<div id="chartContainer" style="height: 300px; width: 100%;"></div>
 	</div>
 </body>
 </html>
