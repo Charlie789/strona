359359fa68d4f265979a97e7d8020ac9d961d04c
Charlie <aplik987@gmail.com>
2019-11-07 20:43:04 +0100

    Przeniesienie skryptu do ciała

--- zadanie4/plan.php
+++ zadanie4/plan.php
@@ -4,29 +4,6 @@
 	<title>Appel</title>
 	<link href="/css/mainstyle.css" rel="stylesheet" type="text/css" />
 	<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-	<script type="text/javascript">
-		window.onload = function () {
-			var chart = new CanvasJS.Chart("chartContainer", {
-				title:{
-					text: "My First Chart in CanvasJS"
-				},
-				data: [
-				{
-					// Change type to "doughnut", "line", "splineArea", etc.
-					type: "column",
-					dataPoints: [
-						{ label: "apple",  y: 10  },
-						{ label: "orange", y: 15  },
-						{ label: "banana", y: 25  },
-						{ label: "mango",  y: 30  },
-						{ label: "grape",  y: 28  }
-					]
-				}
-				]
-			});
-			chart.render();
-		}
-	</script>
 </head>
 
 <body>
@@ -72,6 +49,29 @@
 			</div>
 		</div>
 		<div id="chartContainer" style="height: 300px; width: 100%;"></div>
+		<script type="text/javascript">
+			$function () {
+				var chart = new CanvasJS.Chart("chartContainer", {
+					title:{
+						text: "My First Chart in CanvasJS"
+					},
+					data: [
+					{
+						// Change type to "doughnut", "line", "splineArea", etc.
+						type: "column",
+						dataPoints: [
+							{ label: "apple",  y: 10  },
+							{ label: "orange", y: 15  },
+							{ label: "banana", y: 25  },
+							{ label: "mango",  y: 30  },
+							{ label: "grape",  y: 28  }
+						]
+					}
+					]
+				});
+				chart.render();
+			}
+		</script>
 	</div>
 </body>
 </html>
