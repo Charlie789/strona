4c743959711be95dab0f218ed0750ab47520dc0a
Charlie <admin@kappel.org.pl>
2019-11-08 07:29:18 +0100

    Połączenie wykresu JS z bazą danych

--- zadanie4/plan.php
+++ zadanie4/plan.php
@@ -1,33 +1,78 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT'].'/db_connect.php');
+
+	$data = array();
+        $data1 = array();
+	$data2 = array();
+	$data3 = array();
+	$data4 = array();
+	$data5 = array();
+
+        if ($result = $mysqli->query("SELECT * FROM pomiary_multi")) {
+                while($row = $result->fetch_assoc()) {
+                        $idt = $row["ID"];
+                        $x1 = $row["X1"];
+                        $x2 = $row["X2"];
+                        $x3 = $row["X3"];
+                        $x4 = $row["X4"];
+                        $x5 = $row["X5"];
+                        $time = $row["data_godzina"];
+
+                        array_push($data1, array("label"=>$idt, "y"=>$x1));
+			array_push($data2, array("label"=>$idt, "y"=>$x2));
+			array_push($data3, array("label"=>$idt, "y"=>$x3));
+			array_push($data4, array("label"=>$idt, "y"=>$x4));
+			array_push($data5, array("label"=>$idt, "y"=>$x5));
+			array_push($data, array($idt, $x1, $x2, $x3, $x4, $x5));
+
+                }
+                $result->close();
+        }
+?>
+
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Appel</title>
 	<link href="/css/mainstyle.css" rel="stylesheet" type="text/css" />
 	<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+	<script type="text/javascript">
+		window.onload = function () {
+			var chart = new CanvasJS.Chart("chartContainer", {
+				title:{
+					text: "Historia pomiarów"
+				},
+				data: [
+				{
+					type: "line",
+					dataPoints: <?php echo json_encode($data1, JSON_NUMERIC_CHECK); ?>
+				},
+				{
+                                        type: "line",
+                                        dataPoints: <?php echo json_encode($data2, JSON_NUMERIC_CHECK); ?> 
+                                },
+				{
+                                        type: "line",
+                                        dataPoints: <?php echo json_encode($data3, JSON_NUMERIC_CHECK); ?>
+                                },
+				{
+                                        type: "line",
+                                        dataPoints: <?php echo json_encode($data4, JSON_NUMERIC_CHECK); ?>
+                                },
+				{
+                                        type: "line",
+                                        dataPoints: <?php echo json_encode($data5, JSON_NUMERIC_CHECK); ?>
+                                }
+
+				]
+			});
+			chart.render();
+		}
+	</script>
 </head>
 
 <body>
-	<?php include($_SERVER['DOCUMENT_ROOT'].'/navigation.php');
-	include($_SERVER['DOCUMENT_ROOT'].'/db_connect.php');
-
-	$data = array();
-
-	if ($result = $mysqli->query("SELECT * FROM pomiary_multi")) {
-		while($row = $result->fetch_assoc()) {
-			$idt = $row["ID"];
-			$x1 = $row["X1"];
-			$x2 = $row["X2"];
-			$x3 = $row["X3"];
-			$x4 = $row["X4"];
-			$x5 = $row["X5"];
-			$time = $row["data_godzina"];
-
-			array_push($data, array($idt, $x1, $x2, $x3, $x4, $x5));
-		
-		}
-		$result->close();
-	}
-	?>
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/navigation.php'); ?>
 	<div class="content">
 		<h1>plan mieszkania z czujnikami</h1>
 		<div class="plan">
@@ -49,29 +94,6 @@
 			</div>
 		</div>
 		<div id="chartContainer" style="height: 300px; width: 100%;"></div>
-		<script type="text/javascript">
-			$function () {
-				var chart = new CanvasJS.Chart("chartContainer", {
-					title:{
-						text: "My First Chart in CanvasJS"
-					},
-					data: [
-					{
-						// Change type to "doughnut", "line", "splineArea", etc.
-						type: "column",
-						dataPoints: [
-							{ label: "apple",  y: 10  },
-							{ label: "orange", y: 15  },
-							{ label: "banana", y: 25  },
-							{ label: "mango",  y: 30  },
-							{ label: "grape",  y: 28  }
-						]
-					}
-					]
-				});
-				chart.render();
-			}
-		</script>
 	</div>
 </body>
 </html>
