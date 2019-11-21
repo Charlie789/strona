a4d6b533e2f08f3e2146540edace2f1ddf0b9ff2
Karol Appel <aplik987@gmail.com>
2019-11-21 18:38:52 +0100

    Panel klienta z formularzem

--- zadanie6/panel_klient.php
+++ zadanie6/panel_klient.php
@@ -9,10 +9,32 @@
 
 <body>
 
-	<?php include($_SERVER['DOCUMENT_ROOT'].'/navigation.php'); ?>
+	<?php
+		include($_SERVER['DOCUMENT_ROOT'].'/navigation.php');
+		include($_SERVER['DOCUMENT_ROOT'].'/db_connect.php');
+	?>
 
 	<div class="content">
 		Panel klienta
+
+		<form id="temat_form" method="POST" action="panel_klient_send.php">
+			<label for="zgloszenie"> Zgloszenie : </label>
+			<select id="temat_select" name="temat"     onchange="document.getElementById('selected_text').value=this.options[this.selectedIndex].text">
+				<?php
+					if ($result = $mysqli->query("SELECT * FROM zagadnienia")) {
+						while($row = $result->fetch_assoc()) {
+							$id = $row["id_zagadnienia"];
+							$temat = $row["temat"];
+							print "<option value='$id'>$temat</option>";
+						}
+						$result->close();
+					}
+				?>
+			</select><br>
+			<input type="text" name="tresc" maxlength="5000" size="20">
+			<input type="hidden" name="selected_text" id="selected_text" value="" />
+			<input type="submit" name="send" value="Send"/>
+		</form>
 	</div>
 
 </body>
