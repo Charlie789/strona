a8d571a990c355f3cd434baa9d04c7f726e932a3
Karol Appel <aplik987@gmail.com>
2019-11-23 14:09:20 +0100

    Edycja aktualizowania rekordów przy odpowiadaniu

--- zadanie6/odpowiedz.php
+++ zadanie6/odpowiedz.php
@@ -1,9 +1,10 @@
 <?php
     header("Location: panel_pracownik.php");
     include($_SERVER['DOCUMENT_ROOT'].'/db_connect.php');
+    $worker_id=$_COOKIE['worker_id'];
     $id_post=$_POST['id_post']; // login z formularza
     $odpowiedz=$_POST['tresc']; // hasło z formularza
-    if($mysqli->query("UPDATE `posty` SET post_pracownika = $odpowiedz WHERE id_posty = $id_post") === TRUE) {
+    if($mysqli->query("UPDATE `posty` SET post_pracownika = $odpowiedz, id_pracownik = $worker_id WHERE id_posty = $id_post") === TRUE) {
         echo "dodano do bazy danych";
     } else {
         echo "Error: " . $mysqli->error;
