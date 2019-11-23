c5d1cdc44ea140ee8d9bc140931de7d52c18d97b
Karol Appel <aplik987@gmail.com>
2019-11-23 16:51:30 +0100

    Dodanie ilości odpowiedzi na prawcownika

--- zadanie6/panel_admin.php
+++ zadanie6/panel_admin.php
@@ -21,7 +21,7 @@
 			if ($result = $mysqli->query("SELECT COUNT(*) AS suma FROM posty")) {
 				if($row = $result->fetch_assoc()) {
 					$suma_postow = $row["suma"];
-					print "Ilość zapytań wygenerowanych przez klienta: $suma_postow";
+					print "Ilość zapytań wygenerowanych przez klienta: $suma_postow<br>";
 				}
 			}
 			$result->close();
@@ -32,6 +32,18 @@
 				}
 			}
 			$result->close();
+			echo "<table border='1' width='40%'>";
+			echo "<tr><td width='70%'>Nazwisko</td><td width='30%'>Ilość odpowiedzi</td></tr>";
+			if ($result = $mysqli->query("SELECT p.nazwisko as nazwisko, count(po.id_posty) as suma FROM pracownicy AS p LEFT JOIN posty AS po ON p.id_pracownicy = po.id_pracownik GROUP BY nazwisko")) {
+				if($row = $result->fetch_assoc()) {
+					$pracownik = $row["nazwisko"];
+					$suma_odpowiedzi = $row["suma"];
+					print "<tr><td width='70%'>$pracownik</td><td width='30%'>$suma_odpowiedzi</td></tr>";
+				}
+			}
+			echo "</table>";
+			$result->close();
+			
 		?>
 	</div>
 
