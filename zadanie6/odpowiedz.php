49c27c94bbf924b75418286f3740991f8295ae2e
Charlie <admin@kappel.org.pl>
2019-11-23 14:15:31 +0100

    poprawienie kwerenedy

--- zadanie6/odpowiedz.php
+++ zadanie6/odpowiedz.php
@@ -3,7 +3,7 @@
     $worker_id=$_COOKIE['worker_id'];
     $id_post=$_POST['id_post'];
     $odpowiedz=$_POST['tresc'];
-    if($mysqli->query("UPDATE `posty` SET post_pracownika = $odpowiedz, id_pracownik = $worker_id WHERE id_posty = $id_post") === TRUE) {
+    if($mysqli->query("UPDATE `posty` SET `post_pracownika` = '$odpowiedz', `id_pracownik` = '$worker_id' WHERE id_posty = $id_post") === TRUE) {
         echo '<script type="text/javascript">location.href = "panel_pracownik.php"</script>';
     } else {
         echo "Error: " . $mysqli->error;
