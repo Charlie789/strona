8937ba86b6197dedebf7d0ed53c5385a49445384
Karol Appel <aplik987@gmail.com>
2019-11-23 14:00:41 +0100

    Możliwość odpowiedania klientom przez pracownika

--- zadanie6/panel_pracownik.php
+++ zadanie6/panel_pracownik.php
@@ -13,6 +13,46 @@
 
 	<div class="content">
 		Panel pracownika
+		<br>
+		Posty klientów:
+		<br><br>
+		<div style="height:400px;overflow:auto;">
+			<table border='1' width='95%'>
+			<?php
+			if ($result = $mysqli->query("select p.id_posty, kl.nazwisko, p.post_klienta, pr.nazwisko, p.post_pracownika, p.ocena from posty as p left join pracownicy as pr on p.id_pracownik = pr.id_pracownicy LEFT JOIN klienci as kl on p.id_klient = kl.id_klienci WHERE p.id_zagadnienie=2")) {
+				while($row = $result->fetch_assoc()) {
+					$id_post = $row["id_posty"];
+					$nazwisko_pracownika = $row["pr.nazwisko"];
+					$nazwisko_klienta = $row["kl.nazwisko"];
+					$post_klienta = $row["post_klienta"];
+					$post_pracownika = $row["post_pracownika"];
+					$ocena = $row["ocena"];
+					if(!$post_pracownika){
+						print '<form id="ocena_form" method="POST" action="ocen.php">';
+						print "<tr><td width='10%'>$nazwisko_klienta</td><td width='35%'>$post_klienta</td><td width='35%'>";
+						print '<textarea name="tresc" cols="20" rows="5"></textarea>';
+						// echo '<select id="ocena_select" name="ocena" onchange="document.getElementById(\'wybrana_ocena\').value=this.options[this.selectedIndex].value">';
+						// 	print "<option value='1'>1</option>";
+						// 	print "<option value='2'>2</option>";
+						// 	print "<option value='3'>3</option>";
+						// 	print "<option value='4'>4</option>";
+						// 	print "<option value='5'>5</option>";
+						// echo '</select>';
+						echo "<input type=\"hidden\" name=\"id_post\" id=\"id_post\" value=$id_post />";
+						echo '<input type="submit" name="send" value="odpowiedz"/></form>';
+						echo "</td><td width='10%'>$nazwisko_pracownika</td><td width='10%'>$ocena</td></tr>";
+					} else {
+						print "<tr><td width='10%'>$nazwisko_klienta</td><td width='35%'>$post_klienta</td><td width='35%'>$post_pracownika</td><td width='10%'>$nazwisko_pracownika</td><td width='10%'>$ocena</td></tr>";
+					}
+				}
+				if($ocena=="0") {
+					echo "brak oceny";
+				}
+				$result->close();
+			}
+			print "</table>";
+			?>
+		</div>
 	</div>
 
 </body>
