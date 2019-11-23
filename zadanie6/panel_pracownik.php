929c4d47b315e46892425a473bf1c9b2afa1f70d
Karol Appel <aplik987@gmail.com>
2019-11-23 14:43:02 +0100

    Pokazywanie nazwy aktualnie wybranej kategorii i informowanie o braku postów gdy taka sytuacja wystąpi

--- zadanie6/panel_pracownik.php
+++ zadanie6/panel_pracownik.php
@@ -32,6 +32,9 @@
 				while($row = $result->fetch_assoc()) {
 					$id = $row["id_zagadnienia"];
 					$temat = $row["temat"];
+					if ($selected_zagadnienie == $id){
+						$wybrany_temat = $temat;
+					}
 					print "<option value='$id'>$temat</option>";
 				}
 			}
@@ -40,34 +43,42 @@
 		<input type="hidden" name="selected_zagadnienie" id="selected_zagadnienie" value="1" />
 		<input type="submit" name="send" value="Zatwierdź zagadnienie"/>
 		</form>
+		
+		Kategoria: <?php echo "$wybrany_temat"; ?><br><br>
+		
 		Posty klientów:
 		<br><br>
 		<div style="height:400px;overflow:auto;">
 			<table border='1' width='95%'>
 			<?php
 			if ($result = $mysqli->query("select p.id_posty, kl.nazwisko as nazwisko_klienta, p.post_klienta, pr.nazwisko as nazwisko_pracownika, p.post_pracownika, p.ocena from posty as p left join pracownicy as pr on p.id_pracownik = pr.id_pracownicy LEFT JOIN klienci as kl on p.id_klient = kl.id_klienci WHERE p.id_zagadnienie=$zagadnienie")) {
-				while($row = $result->fetch_assoc()) {
-					$id_post = $row["id_posty"];
-					$nazwisko_pracownika = $row["nazwisko_pracownika"];
-					$nazwisko_klienta = $row["nazwisko_klienta"];
-					$post_klienta = $row["post_klienta"];
-					$post_pracownika = $row["post_pracownika"];
-					$ocena = $row["ocena"];
-					if($ocena=="0") {
-						$ocena = "Brak oceny";
+				if($row = $result->fetch_assoc()){
+					while($row = $result->fetch_assoc()) {
+						$id_post = $row["id_posty"];
+						$nazwisko_pracownika = $row["nazwisko_pracownika"];
+						$nazwisko_klienta = $row["nazwisko_klienta"];
+						$post_klienta = $row["post_klienta"];
+						$post_pracownika = $row["post_pracownika"];
+						$ocena = $row["ocena"];
+						if($ocena=="0") {
+							$ocena = "Brak oceny";
+						}
+						if(!$post_pracownika){
+							print '<form id="ocena_form" method="POST" action="odpowiedz.php">';
+							print "<tr><td width='10%'>$nazwisko_klienta</td><td width='35%'>$post_klienta</td><td width='35%'>";
+							print '<textarea name="tresc" cols="60" rows="5"></textarea>';
+							
+							echo "<input type=\"hidden\" name=\"id_post\" id=\"id_post\" value=$id_post />";
+							echo '<input type="submit" name="send" value="odpowiedz"/></form>';
+							echo "</td><td width='10%'>$nazwisko_pracownika</td><td width='10%'>$ocena</td></tr>";
+						} else {
+							print "<tr><td width='10%'>$nazwisko_klienta</td><td width='35%'>$post_klienta</td><td width='35%'>$post_pracownika</td><td width='10%'>$nazwisko_pracownika</td><td width='10%'>$ocena</td></tr>";
+						}
 					}
-					if(!$post_pracownika){
-						print '<form id="ocena_form" method="POST" action="odpowiedz.php">';
-						print "<tr><td width='10%'>$nazwisko_klienta</td><td width='35%'>$post_klienta</td><td width='35%'>";
-						print '<textarea name="tresc" cols="60" rows="5"></textarea>';
-						
-						echo "<input type=\"hidden\" name=\"id_post\" id=\"id_post\" value=$id_post />";
-						echo '<input type="submit" name="send" value="odpowiedz"/></form>';
-						echo "</td><td width='10%'>$nazwisko_pracownika</td><td width='10%'>$ocena</td></tr>";
-					} else {
-						print "<tr><td width='10%'>$nazwisko_klienta</td><td width='35%'>$post_klienta</td><td width='35%'>$post_pracownika</td><td width='10%'>$nazwisko_pracownika</td><td width='10%'>$ocena</td></tr>";
-					}
+				} else {
+					echo "Brak postów w wybranej kategorii";
 				}
+				
 				$result->close();
 			}
 			print "</table>";
