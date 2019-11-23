6df21020adf9b781c0ccd88f13e035b7c1d870a0
Karol Appel <aplik987@gmail.com>
2019-11-23 14:12:17 +0100

    przekierowanie z js zamiast php

--- zadanie6/odpowiedz.php
+++ zadanie6/odpowiedz.php
@@ -1,11 +1,10 @@
 <?php
-    header("Location: panel_pracownik.php");
     include($_SERVER['DOCUMENT_ROOT'].'/db_connect.php');
     $worker_id=$_COOKIE['worker_id'];
-    $id_post=$_POST['id_post']; // login z formularza
-    $odpowiedz=$_POST['tresc']; // hasło z formularza
+    $id_post=$_POST['id_post'];
+    $odpowiedz=$_POST['tresc'];
     if($mysqli->query("UPDATE `posty` SET post_pracownika = $odpowiedz, id_pracownik = $worker_id WHERE id_posty = $id_post") === TRUE) {
-        echo "dodano do bazy danych";
+        echo '<script type="text/javascript">location.href = "panel_pracownik.php"</script>';
     } else {
         echo "Error: " . $mysqli->error;
     }
