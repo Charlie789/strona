9a9bbd233473670794bb9ba12fb0452df3d32c60
Karol Appel <aplik987@gmail.com>
2019-11-27 22:51:31 +0100

    poprawienie kwerendy

--- zadanie7/weryfikuj.php
+++ zadanie7/weryfikuj.php
@@ -14,7 +14,7 @@
             if($row['password']==$pass){
                 echo '<script type="text/javascript">createCookie(\'zad7_user\', \'',$user,'\');</script>';
                 $mysqli->query("INSERT INTO `logi`(`user_id`, `correct`, `data_czas`) VALUES ('$user_id', '1',  NOW())") === TRUE;
-                if($result2 = $mysqli->query("SELECT * FROM `logi` WHERE user_id=1 order by history_id DESC LIMIT 1,1")) { 
+                if($result2 = $mysqli->query("SELECT * FROM `logi` WHERE user_id=$user_id order by history_id DESC LIMIT 1,1")) { 
                     if($row2 = $result2->fetch_assoc()){
                         $correct = $row['correct'];
                         if ($correct == 1) {
