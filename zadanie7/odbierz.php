75cac03aa38e8a147b7f8c5e7a1832e8d74cefe6
Karol Appel <aplik987@gmail.com>
2019-11-27 22:13:20 +0100

    próba przenoszenia plików do podfolderów

--- zadanie7/odbierz.php
+++ zadanie7/odbierz.php
@@ -1,4 +1,6 @@
 <?php
+    echo '<script src="/js/cookie.js"></script>';
+    $user_id=$_COOKIE['zad7_user_id'];
     $max_rozmiar = 1000;
     if (is_uploaded_file($_FILES['plik']['tmp_name'])) {
         if ($_FILES['plik']['size'] > $max_rozmiar) {
@@ -8,7 +10,7 @@
             if (isset($_FILES['plik']['type'])) {
                 echo 'Typ: '.$_FILES['plik']['type'].'<br/>';
             }
-            move_uploaded_file($_FILES['plik']['tmp_name'],$_SERVER['DOCUMENT_ROOT'].$_FILES['plik']['name']);
+            move_uploaded_file($_FILES['plik']['tmp_name'],$_SERVER['DOCUMENT_ROOT'].'/pliki//'.$_FILES['plik']['name']);
         }
     } else {
         echo 'Błąd przy przesyłaniu danych!';
