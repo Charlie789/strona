e9c3ee15e5850ba98272cc8db2f3226ac1971ada
Karol Appel <aplik987@gmail.com>
2019-12-05 20:30:15 +0100

    przeniesienie kodu chatbota z .js do .php

--- zadanie8/chatbot.php
+++ zadanie8/chatbot.php
@@ -21,7 +21,58 @@
       </form>     </div>
  
     <script src="https://unpkg.com/rivescript@latest/dist/rivescript.min.js"></script>
-    <script src="/js/script.js"></script>
+	<script type="text/javascript">
+	let bot = new RiveScript();
+
+const brains = [
+   '/brain.rive'
+// './another-category-sample.rive
+];
+bot.loadFile(brains).then(botReady).catch(botNotReady);
+
+const message_container = document.querySelector('.messages');
+const form = document.querySelector('form');
+const input_box = document.querySelector('input');
+
+function botOferta(){
+   var oferta = <?php echo "oferta"; ?>;
+   botReply(oferta);
+}
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+   selfReply(input_box.value);
+   input_box.value = '';
+});
+
+function botReply(message){
+   message_container.innerHTML += `<div class='bot'>${message}</div>`;
+   location.href = '#edge';
+}
+
+function selfReply(message){
+   message_container.innerHTML += `<div class="self">${message}</div>`;
+   location.href = '#edge';
+
+   if (message == "oferta"){
+      botOferta();
+   } else {
+      bot.reply("local-user", message).then(function(reply) {
+         botReply(reply);
+      });
+   }
+   
+   
+}
+
+function botReady(){
+   bot.sortReplies();
+   botReply('Hello');
+}
+
+function botNotReady(err){
+   console.log("An error has occurred.", err);
+}</script>
 		
 </div>
 </body>
