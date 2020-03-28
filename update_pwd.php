c5a609d21a07a159da90c075accc72c848a6cff3
Charlie <admin@kappel.org.pl>
2020-03-28 13:17:03 +0100

    poprawka

--- update_pwd.php
+++ update_pwd.php
@@ -2,5 +2,6 @@
 <?php include 'db_connect.php';
 	$username = $_POST['username'];
 	$pwd = $_POST['password'];
-	$result = $mysqli->query("UPDATE Android SET Password = '$pwd' WHERE Android.Username = $username; ");
+	$result = $mysqli->query("UPDATE Android SET Password = '$pwd' 
+WHERE Android.Username = '$username'; ");
 ?>
