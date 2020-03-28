f5c64c78d9945d083315b7411990ab68bb1ded22
Charlie <admin@kappel.org.pl>
2020-03-28 13:23:41 +0100

    poprawka w logowaniu

--- test.php
+++ test.php
@@ -1,12 +1,13 @@
-
 <?php include 'db_connect.php';
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	$result = $mysqli->query("SELECT Role FROM Android where Username='$username' and Password='$password'");
 	$row = $result->fetch_assoc();
-	$data = $row[0];
+	$data = $row['Role'];
 	if($data){
-		echo $data;
+		echo 1;
+	} else {
+		echo 0;
 	}
 	$result->close();
 ?>
