03bd316dc780bb703471cfb1d06b786631363333
Karol Appel <aplik987@gmail.com>
2020-03-28 11:08:20 +0100

    poprawa pkp

--- test.php
+++ test.php
@@ -1,27 +1,12 @@
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta http-equiv="refresh" content="10" />
-	<title>Appel</title>
-	<link href="/css/mainstyle.css" rel="stylesheet" type="text/css" />
-</head>
 
-<body>
-	<?php include($_SERVER['DOCUMENT_ROOT'].'/navigation.php'); ?>
-
-	<div class="content">
-		Android Test
 		<?php include 'db_connect.php';
 			$username = $_POST['username'];
 			$password = $_POST['password'];
 			$result = $mysqli->query("SELECT Role FROM Android where Username='$username' and Password='$password'");
 			$row = $result->fetch_assoc();
-			$data = $row['Role'];
+			$data = $row[0];
 			if($data){
 				echo $data;
 			}
 			$result->close();
 		?>
-	</div>
-</body>
-</html>
