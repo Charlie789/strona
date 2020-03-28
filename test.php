88d394002bb60c6afe20277f9661e24a79a15388
Karol Appel <aplik987@gmail.com>
2020-03-28 13:13:20 +0100

    dodanie resetowania hasla

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 
-		<?php include 'db_connect.php';
-			$username = $_POST['username'];
-			$password = $_POST['password'];
-			$result = $mysqli->query("SELECT Role FROM Android where Username='$username' and Password='$password'");
-			$row = $result->fetch_assoc();
-			$data = $row[0];
-			if($data){
-				echo $data;
-			}
-			$result->close();
-		?>
+<?php include 'db_connect.php';
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$result = $mysqli->query("SELECT Role FROM Android where Username='$username' and Password='$password'");
+	$row = $result->fetch_assoc();
+	$data = $row[0];
+	if($data){
+		echo $data;
+	}
+	$result->close();
+?>
